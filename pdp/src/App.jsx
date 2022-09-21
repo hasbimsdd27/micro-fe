b027aeade8e40a4a22b00f4142a25fb35f8a7f0f
Hasbi Musaddad <musaddadhasbi@gmail.com>
2022-09-21 21:27:50 +0700

    initial commit

---
+++ pdp/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+
+import Footer from "home/Footer";
+import Header from "home/Header";
+import PDPContent from "./PDPContent";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () => (
+  <Router>
+    <div className="text-3xl mx-auto max-w-6xl">
+      <Header />
+
+      <div className="my-10">
+        <Switch>
+          <Route path="/product/:id" component={PDPContent} />
+        </Switch>
+      </div>
+      <Footer />
+    </div>
+  </Router>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
